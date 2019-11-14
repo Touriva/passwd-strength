ae327fa269ab70c86d4866e8a376d63f3c50f11f
Michael Toutonghi <toutonghi.michael@gmail.com>
2019-11-14 17:30:12 +0100

    Remove unessesary use of node-specific functions to allow module to be called from React

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 "use strict";
 
-var fs = require('fs');
-var path = require('path');
 var passwdStrength = require('./passwd-strength');
-var passwordsTxt = null;
+var passwords = require('./passwords')
 
 module.exports = function main(passwd) {
-
     passwd = '' + passwd; // ensure it's a string or our toLowerCase() below will fail.
 
-    if (passwordsTxt === null) {
-        passwordsTxt = fs.readFileSync(path.join(__dirname, 'passwords.txt')).toString().split("\n");
-    }
-
-    if (passwordsTxt.indexOf(passwd.toLowerCase()) !== -1) {
+    if (passwords.includes(passwd)) {
         return 0;
     }
 
